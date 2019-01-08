be931e308619b870682af60c23222558205d5ca0
Jarry Shaw <JarryShaw@users.noreply.github.com>
2019-01-08 23:12:30 +0800

    New distribution [0.4.1]  - revised shell scripts  - updated README  - considered syntax breakage in Python 3.6/3.7 (but better use standalone ast library to do so)

--- Formula/f2format.rb
+++ Formula/f2format.rb
@@ -1,11 +1,11 @@
 class F2format < Formula
   include Language::Python::Virtualenv
 
-  version "0.4.0.post2"
+  version "0.4.1"
   desc "Back-port compiler for Python 3.6 f-string literals"
   homepage "https://github.com/JarryShaw/f2format#f2format"
-  url "https://files.pythonhosted.org/packages/44/5d/46ad0cc7d327dfbdc3f6aeb82b242e09dd19606585c0196ebc7ac01a53a3/f2format-0.4.0.post2.tar.gz"
-  sha256 "96f06428b29e415a162ced5e446b72bd5910842f6f02055f4b02578f644e2250"
+  url "https://files.pythonhosted.org/packages/a1/e0/1a07cbc387dc4af5307b829696d06e0308d1f42935ec40c71a7f3978c852/f2format-0.4.1.tar.gz"
+  sha256 "99bbfe031d5e4b5f3733ea94586fc6baea96d3b1823c58c88ab6d01b6e96a49e"
 
   head "https://github.com/JarryShaw/f2format.git", :branch => "master"
 
@@ -14,18 +14,18 @@ class F2format < Formula
   depends_on "python"
 
   resource "pathlib2" do
-    url "https://files.pythonhosted.org/packages/db/a8/7d6439c1aec525ed70810abee5b7d7f3aa35347f59bc28343e8f62019aa2/pathlib2-2.3.2.tar.gz"
-    sha256 "8eb170f8d0d61825e09a95b38be068299ddeda82f35e96c3301a8a5e7604cb83"
+    url "https://files.pythonhosted.org/packages/bf/d7/a2568f4596b75d2c6e2b4094a7e64f620decc7887f69a1f2811931ea15b9/pathlib2-2.3.3.tar.gz"
+    sha256 "25199318e8cc3c25dcb45cbe084cc061051336d5a9ea2a12448d3d8cb748f742"
   end
 
   resource "six" do
-    url "https://files.pythonhosted.org/packages/16/d8/bc6316cf98419719bd59c91742194c111b6f2e85abac88e496adefaf7afe/six-1.11.0.tar.gz"
-    sha256 "70e8a77beed4562e7f14fe23a786b54f6296e34344c23bc42f07b15018ff98e9"
+    url "https://files.pythonhosted.org/packages/dd/bf/4138e7bfb757de47d1f4b6994648ec67a51efe58fa907c1e11e350cddfca/six-1.12.0.tar.gz"
+    sha256 "d16a0141ec1a18405cd4ce8b4613101da75da0e9a7aec5bdd4fa804d0e0eba73"
   end
 
   resource "typed-ast" do
-    url "https://files.pythonhosted.org/packages/52/cf/2ebc7d282f026e21eed4987e42e10964a077c13cfc168b42f3573a7f178c/typed-ast-1.1.0.tar.gz"
-    sha256 "57fe287f0cdd9ceaf69e7b71a2e94a24b5d268b35df251a88fef5cc241bf73aa"
+    url "https://files.pythonhosted.org/packages/9d/f8/78010480255fc6a1e545d1e74389128c1a4f11905b9ab16a7ef6c956c704/typed-ast-1.1.1.tar.gz"
+    sha256 "6cb25dc95078931ecbd6cbcc4178d1b8ae8f2b513ae9c3bd0b7f81c2191db4c6"
   end
 
   def install
