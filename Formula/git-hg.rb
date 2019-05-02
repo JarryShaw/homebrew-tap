21914efbe09d94b045ffc517bb6ad4d824f9b10d
Jarry Shaw <JarryShaw@users.noreply.github.com>
2019-05-03 01:55:01 +0800

    git-hg: 2019.5.3

--- Formula/git-hg.rb
+++ Formula/git-hg.rb
@@ -3,12 +3,21 @@ class GitHg < Formula
   homepage "https://github.com/cosmin/git-hg"
   url "https://github.com/cosmin/git-hg.git",
     :revision => "16b573ed0567e89348ba1d066e6afe5bb48b916c"
+  version "2019.5.3"
 
   depends_on "mercurial"
   depends_on "python"
 
   def install
     bin.install "bin/git-hg"
+
+    # install hg-fast-export.(sh|py)
+    bin.install "fast-export/hg-fast-export.sh"
+    bin.install "fast-export/hg-fast-export.py"
+    bin.install "fast-export/hg2git.py"
+
+    # make alias
+    ln_s bin/"hg-fast-export.sh", bin/"hg-fast-export"
   end
 
   test do
