4bb7a513f18cdeaf761cc3e55d9304297927454b
Jarry Shaw <jarryshaw@icloud.com>
2019-07-13 17:33:06 +0800

    noob: 0.2  * fixed dependency issue  * revised formula

--- Formula/noob.rb
+++ Formula/noob.rb
@@ -6,12 +6,47 @@ class Noob < Formula
   url "https://github.com/zmwangx/homebrew-npm-noob/archive/v0.2.tar.gz"
   sha256 "15e2ac4cbf3e549f62199e3b0e3c8d5fbc94043a7aba1b04786f37dffc41de11"
 
+  head "https://github.com/zeek/package-manager.git", :branch => "master"
+
   depends_on "python"
 
+  resource "MarkupSafe" do
+    url "https://files.pythonhosted.org/packages/b9/2e/64db92e53b86efccfaea71321f597fa2e1b2bd3853d8ce658568f7a13094/MarkupSafe-1.1.1.tar.gz"
+    sha256 "29872e92839765e546828bb7754a68c418d927cd064fd4708fab9fe9c8bb116b"
+  end
+
+  resource "certifi" do
+    url "https://files.pythonhosted.org/packages/c5/67/5d0548226bcc34468e23a0333978f0e23d28d0b3f0c71a151aef9c3f7680/certifi-2019.6.16.tar.gz"
+    sha256 "945e3ba63a0b9f577b1395204e13c3a231f9bc0223888be653286534e5873695"
+  end
+
+  resource "chardet" do
+    url "https://files.pythonhosted.org/packages/fc/bb/a5768c230f9ddb03acc9ef3f0d4a3cf93462473795d18e9535498c8f929d/chardet-3.0.4.tar.gz"
+    sha256 "84ab92ed1c4d4f16916e05906b6b75a6c0fb5db821cc65e70cbd64a3e2a5eaae"
+  end
+
+  resource "idna" do
+    url "https://files.pythonhosted.org/packages/ad/13/eb56951b6f7950cadb579ca166e448ba77f9d24efc03edd7e55fa57d04b7/idna-2.8.tar.gz"
+    sha256 "c357b3f628cf53ae2c4c05627ecc484553142ca23264e593d327bcde5e9c3407"
+  end
+
+  resource "Jinja2" do
+    url "https://files.pythonhosted.org/packages/93/ea/d884a06f8c7f9b7afbc8138b762e80479fb17aedbbe2b06515a12de9378d/Jinja2-2.10.1.tar.gz"
+    sha256 "065c4f02ebe7f7cf559e49ee5a95fb800a9e4528727aec6f24402a5374c65013"
+  end
+
+  resource "requests" do
+    url "https://files.pythonhosted.org/packages/01/62/ddcf76d1d19885e8579acb1b1df26a852b03472c0e46d2b959a714c90608/requests-2.22.0.tar.gz"
+    sha256 "11e007a8a2aa0323f5a921e9e6a2d7e4e67d9877e85773fba9ba6419025cbeb4"
+  end
+
+  resource "urllib3" do
+    url "https://files.pythonhosted.org/packages/4c/13/2386233f7ee40aa8444b47f7463338f3cbdf00c316627558784e3f542f07/urllib3-1.25.3.tar.gz"
+    sha256 "dbe59173209418ae49d485b87d1681aefa36252ee85884c31346debd19463232"
+  end
+
   def install
-    virtualenv_create(libexec, "python3")
-    system libexec/"bin/pip", "install", "."
-    bin.install_symlink libexec/"bin/noob"
+    virtualenv_install_with_resources
     man1.install "docs/noob.1"
   end
 
--- Generator/scripts/noob.py
+++ Generator/scripts/noob.py
@@ -1,20 +1,87 @@
 # -*- coding: utf-8 -*-
 # originally from https://raw.githubusercontent.com/zmwangx/homebrew-npm-noob/master/Formula/noob.rb
 
+import hashlib
 import os
+import re
 import subprocess
+import sys
 
-formula = subprocess.check_output(['brew', 'formula', 'noob']).decode().strip()
+import bs4
+import requests
 
-context = list()
-with open(formula) as file:
-    for line in file:
-        context.append(line)
-        if 'depends_on "python3"' in line:
-            context.append('\n')
-            context.append('  conflicts_with "noob", :because => "it is now integrated with homebrew-core"\n')
+for line in subprocess.check_output(['pip', 'freeze']).decode().splitlines():
+    match = re.match(r"homebrew-npm-noob==(.*)", line, re.IGNORECASE)
+    if match is not None:
+        VERSION = match.groups()[0]
 
-FORMULA = ''.join(context)
+NOOB_URL = f'https://github.com/zmwangx/homebrew-npm-noob/archive/v{VERSION}.tar.gz'
+NOOB_SHA = hashlib.sha256(requests.get(NOOB_URL).content).hexdigest()
+
+_data_pkgs = dict()
+
+
+def _fetch_dependency(package):
+    dependencies = _data_pkgs.get(package)
+    if dependencies is not None:
+        return dependencies
+
+    argv = [sys.executable, '-m', 'pip', 'show', package]
+
+    _deps_pkgs = dict()
+    requirements = set()
+    for line in subprocess.check_output(argv).decode().strip().splitlines():
+        match = re.match(r"Requires: (.*)", line)
+        if match is not None:
+            requirements = set(match.groups()[0].split(', '))
+            break
+
+    for item in filter(None, requirements):
+        _deps_pkgs[item] = _fetch_dependency(item)
+    _data_pkgs.update(_deps_pkgs)
+
+    return _deps_pkgs
+
+
+def _list_dependency(dependencies):
+    _list_pkgs = list()
+    for package, deps_pkgs in dependencies.items():
+        _list_pkgs.append(package)
+        _list_pkgs.extend(_list_dependency(deps_pkgs))
+    return _list_pkgs
+
+
+_deps_list = _list_dependency(_fetch_dependency('homebrew-npm-noob'))
+
+args = ['poet', '--single']
+args.extend(sorted(set(_deps_list)))
+NOOB = subprocess.check_output(args).decode().strip()
+
+FORMULA = f'''\
+class Noob < Formula
+  include Language::Python::Virtualenv
+
+  desc "Generate Homebrew formulae for npm packages"
+  homepage "https://github.com/zmwangx/homebrew-npm-noob"
+  url "{NOOB_URL}"
+  sha256 "{NOOB_SHA}"
+
+  head "https://github.com/zeek/package-manager.git", :branch => "master"
+
+  depends_on "python"
+
+  {NOOB}
+
+  def install
+    virtualenv_install_with_resources
+    man1.install "docs/noob.1"
+  end
+
+  test do
+    assert_match "class Svgo < Formula", shell_output("#{{bin}}/noob svgo")
+  end
+end
+'''
 
 with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'Formula',
                        f'{os.path.splitext(os.path.basename(__file__))[0]}.rb'), 'w') as file:
