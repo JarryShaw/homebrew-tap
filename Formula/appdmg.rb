ce17491f374b3239763ddd376cb5a1ddaf2cb00f
Jarry Shaw <jarryshaw@icloud.com>
2019-07-13 17:31:48 +0800

    appdmg: 0.6.0 (new formula)

---
+++ Formula/appdmg.rb
@@ -0,0 +1,19 @@
+require "language/node"
+
+class Appdmg < Formula
+  desc "Generate beautiful DMG-images for your OS X applications"
+  homepage "https://github.com/LinusU/node-appdmg#readme"
+  url "https://registry.npmjs.org/appdmg/-/appdmg-0.6.0.tgz"
+  sha256 "a1faebdec4ed269a9ed159e45634df6abac5fe734970f592ca2c9d649710f48c"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    system bin/"appdmg", "--help"
+  end
+end
