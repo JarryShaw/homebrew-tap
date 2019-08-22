728ee5156d14ee384c83cb5d5f757da76e6802b2
Jarry Shaw <jarryshaw@icloud.com>
2019-08-23 00:26:42 +0800

    sass: 1.22.10 (new formula)

---
+++ Formula/sass.rb
@@ -0,0 +1,28 @@
+require "language/node"
+
+class Sass < Formula
+  desc "A pure JavaScript implementation of Sass"
+  homepage "https://github.com/sass/dart-sass"
+  url "https://registry.npmjs.org/sass/-/sass-1.22.10.tgz"
+  sha256 "ecea121912cc18b742ae62e7473be26ac3783d95ce1f304deecc80e65a676a3a"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *Language::Node.std_npm_install_args(libexec)
+    bin.install_symlink Dir["#{libexec}/bin/*"]
+  end
+
+  test do
+    (testpath/"test.scss").write <<~EOS
+      div {
+        img {
+          border: 0px;
+        }
+      }
+    EOS
+
+    assert_equal "div img{border:0px}",
+    shell_output("#{bin}/sass --style=compressed test.scss").strip
+  end
+end
---
+++ Generator/scripts/sass.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import subprocess
+
+context = list()
+for line in subprocess.check_output(['noob', 'sass'], encoding='utf-8').splitlines():
+    if re.match(r'\s*version ".+?"\s*', line):
+        continue
+    if line.strip() == 'raise "Test not implemented."':
+        line = os.linesep.join([
+            '    (testpath/"test.scss").write <<~EOS',
+            '      div {',
+            '        img {',
+            '          border: 0px;',
+            '        }',
+            '      }',
+            '    EOS',
+            '',
+            '    assert_equal "div img{border:0px}",',
+            '    shell_output("#{bin}/sass --style=compressed test.scss").strip',
+        ])
+    context.append(line)
+
+FORMULA = os.linesep.join(context)
+
+with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'Formula',
+                       f'{os.path.splitext(os.path.basename(__file__))[0]}.rb'), 'w') as file:
+    print(FORMULA, file=file)
