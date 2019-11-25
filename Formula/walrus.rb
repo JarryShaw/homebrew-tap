ce69857067c2cdf531e5ba7f064b47436c771cbc
Jarry Shaw <jarryshaw@icloud.com>
2019-11-25 23:57:09 +0800

    walrus: 0.1.0

---
+++ Formula/walrus.rb
@@ -0,0 +1,45 @@
+class Walrus < Formula
+  include Language::Python::Virtualenv
+
+  desc "Backport compiler for Python 3.8 assignment expressions"
+  homepage "https://github.com/JarryShaw/walrus#walrus"
+  url "https://github.com/JarryShaw/walrus/archive/v0.1.0.tar.gz"
+  sha256 "a07bc62db75f8814c6c63feeb4aa7a1347e315fdee5c50a647cecadf2c684e5f"
+
+  head "https://github.com/JarryShaw/walrus.git", :branch => "master"
+
+  depends_on "python"
+
+  resource "parso" do
+    url "https://files.pythonhosted.org/packages/21/40/615957db4d178b7504c87b1a5b85fa5945b0b4fa5f5a845e31fc7aad6018/parso-0.5.1.tar.gz"
+    sha256 "666b0ee4a7a1220f65d367617f2cd3ffddff3e205f3f16a0284df30e774c2a9c"
+  end
+
+  resource "tbtrim" do
+    url "https://files.pythonhosted.org/packages/5d/79/617749a3e689dbb741da15cf3134fd52a47e3227d878a48573ece71df043/tbtrim-0.2.1.tar.gz"
+    sha256 "b0810edfb5dcf94c5fe3335a8a8e18ae38a411f6ff6afca188c66ac72444218f"
+  end
+
+  def install
+    rm "setup.py"
+    cp "scripts/setup.pypi.py", "setup.py"
+    virtualenv_install_with_resources
+
+    man1.install "share/walrus.1"
+    bash_completion.install "share/walrus.bash-completion"
+  end
+
+  test do
+    (testpath/"test.py").write <<~EOS
+      a = (b := 1)
+      print(a, b)
+    EOS
+
+    std_output = <<~EOS
+      1 1
+    EOS
+
+    system bin/"walrus", "--no-archive", "test.py"
+    assert_match std_output, shell_output("#{Formula["python3"].bin}/python test.py")
+  end
+end
