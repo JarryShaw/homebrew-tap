cd81bc2dfc0afd1f09e8ab02288bb615198f2442
GitHub Action <action@github.com>
2022-02-09 00:22:06 +0000

    Regular update at Wed Feb  9 00:22:06 UTC 2022

--- Formula/macdaily.rb
+++ Formula/macdaily.rb
@@ -39,8 +39,8 @@ class Macdaily < Formula
   end
 
   resource "pathlib2" do
-    url "https://files.pythonhosted.org/packages/df/16/e9d6bcf1aed52a55bc1696324ab22586716053b3e97b85266e0f3ad36bae/pathlib2-2.3.6.tar.gz"
-    sha256 "7d8bcb5555003cdf4a8d2872c538faa3a0f5d20630cb360e518ca3b981795e5f"
+    url "https://files.pythonhosted.org/packages/d0/74/0fefd1dd0c159cf91bdc43f9dbfdaeadb231d9bfc9d7111c4e03e893291b/pathlib2-2.3.7.post0.tar.gz"
+    sha256 "3207eb068c994a04998c42a34ae01c81c8d1d50eb88d7371f57fee61d1822302"
   end
 
   resource "six" do
