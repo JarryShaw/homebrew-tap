05c4cf064775952d6aa30729b14d65a2a2b6e357
GitHub Action <action@github.com>
2022-08-21 00:25:19 +0000

    Regular update at Sun Aug 21 00:25:19 UTC 2022

--- Formula/vermin.rb
+++ Formula/vermin.rb
@@ -3,8 +3,8 @@ class Vermin < Formula
 
   desc "Concurrently detect the minimum Python versions needed to run code"
   homepage "https://github.com/netromdk/vermin"
-  url "https://github.com/netromdk/vermin/archive/v1.4.1.tar.gz"
-  sha256 "ee69d5e84f0d446e0d6574ec60c428798de6e6c8d055589f65ac02f074a7da25"
+  url "https://github.com/netromdk/vermin/archive/v1.4.2.tar.gz"
+  sha256 "c9a69420b610bfb25d5a2abd7da6edf0ae4329481a857ef6c5d71f602ed5c63d"
 
   head "https://github.com/netromdk/vermin.git", branch: "master"
 
