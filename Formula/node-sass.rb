947d705c329bb08b36fa0a40d6fd42e86686a599
GitHub Action <action@github.com>
2022-09-08 00:31:42 +0000

    Regular update at Thu Sep  8 00:31:42 UTC 2022

--- Formula/node-sass.rb
+++ Formula/node-sass.rb
@@ -3,17 +3,17 @@ class NodeSass < Formula
 
   desc "JavaScript implementation of a Sass compiler"
   homepage "https://github.com/sass/dart-sass"
-  url "https://registry.npmjs.org/sass/-/sass-1.54.5.tgz"
-  sha256 "b772ef2acccd3325a847bd4efb1020ae6b4ba92433b1e57171e13c95f72dee9a"
+  url "https://registry.npmjs.org/sass/-/sass-1.54.8.tgz"
+  sha256 "3a96a7b8101274cde4281f8132e7912d197dd2ff347036f0492e604536fe5c30"
   license "MIT"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "c7a29f887aeff0549010ccf091e698ef8020ace1dff272c905f4f9516a3e642f"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "c7a29f887aeff0549010ccf091e698ef8020ace1dff272c905f4f9516a3e642f"
-    sha256 cellar: :any_skip_relocation, monterey:       "c7a29f887aeff0549010ccf091e698ef8020ace1dff272c905f4f9516a3e642f"
-    sha256 cellar: :any_skip_relocation, big_sur:        "c7a29f887aeff0549010ccf091e698ef8020ace1dff272c905f4f9516a3e642f"
-    sha256 cellar: :any_skip_relocation, catalina:       "c7a29f887aeff0549010ccf091e698ef8020ace1dff272c905f4f9516a3e642f"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:   "3ca6801566764b516b31d4814e581c1a8157530071676e2c4a174fe0f875abf4"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "f39d7dd6af57acaf99293fa1632181e4c3037adba05287c8df9b5d7bea06340b"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur:  "f39d7dd6af57acaf99293fa1632181e4c3037adba05287c8df9b5d7bea06340b"
+    sha256 cellar: :any_skip_relocation, monterey:       "f39d7dd6af57acaf99293fa1632181e4c3037adba05287c8df9b5d7bea06340b"
+    sha256 cellar: :any_skip_relocation, big_sur:        "f39d7dd6af57acaf99293fa1632181e4c3037adba05287c8df9b5d7bea06340b"
+    sha256 cellar: :any_skip_relocation, catalina:       "f39d7dd6af57acaf99293fa1632181e4c3037adba05287c8df9b5d7bea06340b"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "c314bfce533d004141ee580902dcde3ac2e96c475853d3099056b3e8b0fb9e17"
   end
 
   depends_on "homebrew/core/node"
