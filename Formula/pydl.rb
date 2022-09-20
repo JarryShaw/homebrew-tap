d4bdd43e671967236cf237843746d372c37ac034
GitHub Action <action@github.com>
2022-09-20 00:34:35 +0000

    Regular update at Tue Sep 20 00:34:35 UTC 2022

--- Formula/pydl.rb
+++ Formula/pydl.rb
@@ -4,7 +4,7 @@ class Pydl < Formula
   desc "Video downloader powered by jarryshaw.me"
   homepage "https://jarryshaw.me"
   url "https://github.com/JarryShaw/pydl.git"
-  version "2022.9.19"
+  version "2022.9.20"
 
   depends_on "homebrew/core/aria2"
   depends_on "homebrew/core/python@3.9"
