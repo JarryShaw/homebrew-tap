8323b63313783b8d60f64ec167f7af147f895f7b
GitHub Action <action@github.com>
2022-09-22 00:29:06 +0000

    Regular update at Thu Sep 22 00:29:06 UTC 2022

--- Formula/basher.rb
+++ Formula/basher.rb
@@ -3,7 +3,7 @@ class Basher < Formula
   homepage "https://github.com/basherpm/basher"
   url "https://github.com/basherpm/basher.git",
     revision: "503d7393627fd8f92ac450c6bc43c4de1be1f85f"
-  version "2022.9.21"
+  version "2022.9.22"
 
   depends_on "homebrew/core/bash"
   depends_on "homebrew/core/coreutils"
